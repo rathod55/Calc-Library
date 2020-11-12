b56082a95a6960fb26798b33801fa216f4e6b935
rathod55 <rathod55developer@gmail.com>
2020-11-13 00:39:20 +0530

    Initial Commit

---
+++ calclibr/src/main/java/com/abhirathore/calclibr/Calc_Library.java
@@ -0,0 +1,25 @@
+package com.abhirathore.calclibr;
+
+public class Calc_Library {
+
+    public static float add(float num1, float num2)
+    {
+        float result_add=num1+num2;
+        return result_add;
+    }
+    public static float sub(float num1,float num2)
+    {
+        float result_sub=num1-num2;
+        return result_sub;
+    }
+    public static float mul(float num1, float num2 )
+    {
+        float result_mul=num1 * num2;
+        return result_mul;
+    }
+    public static float div(float num1,float num2)
+    {
+        float result_div=num1/num2;
+        return result_div;
+    }
+}
